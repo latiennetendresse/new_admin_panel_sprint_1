61db5b2b545971f94ef0bf44077dd2ee71b3d2ab
Dary <d.nevzorova@unlim.group>
2023-04-15 18:16:05 +0500

    created app "movies"

---
+++ movies/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmwork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
+
+    # Отображение полей в списке
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+
+    # Фильтрация в списке
+    list_filter = ('type', 'rating',)
+
+    # Поиск по полям
+    search_fields = ('title', 'description', 'id')
